e393a39f46e11ce486ecd3c00a41cf4ad7042cf0
AntoniellaMonsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2021-11-19 08:25:28 -0300

    Create 1005.cs

---
+++ Lista 03/1005.cs
@@ -0,0 +1,19 @@
+using System;
+
+class MainClass {
+
+    public static void Main(string[] args)
+    {
+        double a, b, result;
+        double pa = 3.5;
+        double pb = 7.5;
+    
+        a = Double.Parse(Console.ReadLine());
+        b = Double.Parse(Console.ReadLine());
+
+        result = (a * pa + b * pb) / (pa + pb);
+
+        Console.WriteLine($"MEDIA = {result:0.00000}");
+    }
+
+}
