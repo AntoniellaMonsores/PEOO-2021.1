b87c53f8f48870c2fe1a27817d377b2d9f17ec59
AntoniellaMonsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2021-11-19 08:34:57 -0300

    Create 1011.cs

---
+++ Lista 03/1011.cs
@@ -0,0 +1,16 @@
+using System;
+
+class MainClass
+{
+
+    public static void Main(string[] args)
+    {
+        int r;
+        double volume;
+
+        r = int.Parse(Console.ReadLine());
+        volume = (4 / 3.0) * 3.14159 * Math.Pow(r, 3);
+
+        Console.WriteLine($"VOLUME = {volume:0.000}");
+    }
+}
