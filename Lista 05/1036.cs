05e6be8a496220a9f37e8d66052d3aed30d7c575
Antoniella <78799880+AntoniellaMonsores@users.noreply.github.com>
2021-11-21 11:46:31 +0000

    Lista 05/

---
+++ Lista 05/1036.cs
@@ -0,0 +1,30 @@
+using System;
+
+class MainClass
+{
+    public static void Main()
+    {
+        double a, b, c, rd, r1, r2;
+        string[] num;
+
+        num = Console.ReadLine().Split();
+
+        a = Double.Parse(num[0]);
+        b = Double.Parse(num[1]);
+        c = Double.Parse(num[2]);
+
+        rd = Math.Pow(b, 2) - 4 * a * c;
+
+        if (a == 0 || rd <= 0)
+            Console.WriteLine("Impossivel calcular");
+
+        else {
+            rd = Math.Sqrt(rd);
+            r1 = (-b + rd) / (2 * a);
+            r2 = (-b - rd) / (2 * a);
+
+            Console.WriteLine($"R1 = {r1:0.00000}");
+            Console.WriteLine($"R2 = {r2:0.00000}");
+        }
+    }
+}
