4e014940560ad37336c4cefcdb423cf76e6ca06d
Antoniella <78799880+AntoniellaMonsores@users.noreply.github.com>
2021-11-26 11:41:41 +0000

    .replit

---
+++ Lista 06/1080.cs
@@ -0,0 +1,15 @@
+using System;
+
+class MainClass {
+    public static void Main(string[] args)
+    {
+        int[] arr = new int[100];
+
+        for (int i = 0; i < 100; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        
+    }
+}
