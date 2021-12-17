8b02c0deb251c31bb90c7beddb3350a144a2865e
AntoniellaMonsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2021-12-17 09:11:16 -0300

    Update 2670.cs

--- Lista 05/2670.cs
+++ Lista 05/2670.cs
@@ -2,8 +2,8 @@ using System;
 
 class MainClass {
 
-    public static void Main(string[] args)
-    {
+    public static void Main(string[] args) {
+        
         int a1, a2, a3;
         int t1, t2, t3;
 
@@ -15,14 +15,11 @@ class MainClass {
         t2 = 2 * (a1 * 1 + a3 * 1);
         t3 = 2 * (a1 * 2 + a2 * 1);
 
-        if (t1 > t2) {
-            t1 = t2;
-        }
-
-        if (t1 > t3) {
-            t1 = t3;
-        }
+        if (t1 > t2) t1 = t2;
+        if (t1 > t3) t1 = t3;
 
         Console.WriteLine(t1);
+        
     }
+    
 }
