3251372bdc31cf68d754b77955bedeb88823e149
AntoniellaMonsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2021-12-17 09:12:37 -0300

    Update ex03.cs

--- Lista 02/ex03.cs
+++ Lista 02/ex03.cs
@@ -1,9 +1,9 @@
 using System;
 
-class MainClass
-{
-    public static void Main(string[] args)
-    {
+class MainClass {
+    
+    public static void Main(string[] args) {
+        
         int n1, n2;
         int p1 = 2;
         int p2 = 3;
@@ -17,5 +17,7 @@ class MainClass
         int mediaParcial = (n1 * p1 + n2 * p2) / (p1 + p2);
 
         Console.WriteLine($"\nMédia parcial = {mediaParcial}");
+        
     }
+    
 }
