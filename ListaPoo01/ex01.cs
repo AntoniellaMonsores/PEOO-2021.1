636af3125294f08db3d4d36fedf95611b5c4c68a
Antoniella Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2021-12-18 19:29:28 +0000

    Update ex01.cs

--- ListaPoo01/ex01.cs
+++ ListaPoo01/ex01.cs
@@ -1,1 +1,29 @@
+using System;
 
+class Circulo {
+
+    public double raio;
+    public double pi;
+
+    public double CalcArea() {
+        return (pi * pi) * raio;
+    }
+
+    public double CalcCircunferencia() {
+        return 2 * pi * raio;
+    }
+}
+
+class MainClass {
+
+    public static void Main() {
+
+        Circulo x = new Circulo();
+        x.raio = 2;
+        x.pi = 3.14159;
+
+        Console.WriteLine($"Área do círculo: {x.CalcArea():0.00}");
+        Console.WriteLine($"Circunferência do círculo: {x.CalcCircunferencia():0.00}");
+    }
+
+}
