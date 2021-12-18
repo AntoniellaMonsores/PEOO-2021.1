b4c05418ffb2a12b5cfadabbd1887bb010721468
AntoniellaMonsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2021-12-18 17:13:20 -0300

    Update ex03.cs

--- ListaPoo01/ex03.cs
+++ ListaPoo01/ex03.cs
@@ -1,1 +1,27 @@
+using System;
 
+class Viagem {
+
+    public int d; // distância
+    public int h; // horas
+    public int m; // minutos
+
+    public int VelMedia() {
+        return d / h + (m / 60);
+    }
+    
+ }
+ 
+ class MainClass {
+ 
+    public static void {
+    
+        Viagem x = new Viagem();
+        x.d = 200;
+        x.h = 2;
+        x.m = 30;
+
+        Console.WriteLine($"Velocidade: {x.VelMedia} km/h");
+    }
+    
+}
