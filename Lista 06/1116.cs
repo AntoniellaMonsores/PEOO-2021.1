3e42c410357b4d0b21a3336901ef58da6ec0e7d5
Antoniella Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2021-12-24 10:50:07 +0000

    Create 1116.cs

---
+++ Lista 06/1116.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Program {
+
+    public static void Main(string[] args) {
+
+        int n;
+        double x;
+        double y;
+        string values;
+
+        n = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < n; i++) 
+        {
+            values = Console.ReadLine().Split();
+
+            x = double.Parse(values[0]);
+            y = double.Parse(values[1]);
+
+            if (y == 0) 
+                Console.WriteLine("divisao impossivel");
+            else
+                Console.WriteLine($"{(x / y):0.0}");
+        }
+
+    }
+
+}
