f0cf34ac6b5b55fba7786c71044f241545c1b8fc
Antoniella Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2021-12-29 21:34:33 +0000

    Create ex04.cs

---
+++ ListaPoo01/ex04.cs
@@ -0,0 +1,22 @@
+using System;
+
+class MainClass {
+
+    public static void Main(string[] args) {
+
+
+
+    }
+
+}
+
+class Cinema {
+
+    public string d;
+    public int h;
+
+    public double ValorEntrada() {
+
+    }
+
+}
