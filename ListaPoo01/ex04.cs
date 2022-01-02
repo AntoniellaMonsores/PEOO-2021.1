76e277fc6acf6d1a40c29b1f45ddd7d8680d67b6
Antoniella Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2022-01-02 13:45:58 +0000

    Update main.exe

--- ListaPoo01/ex04.cs
+++ ListaPoo01/ex04.cs
@@ -5,7 +5,14 @@ class MainClass {
     public static void Main(string[] args) {
 
         Cinema x = new Cinema();
-        x.d = Console.Readline().ToLower();
+
+        Console.WriteLine("Informações da Sessão");
+
+        Console.Write("Dia: ");
+        // for cases as "Segunda" or "segunda-feira"
+        x.d = Console.ReadLine().ToLower().Split("-")[0];
+
+        Console.Write("Horário: ");
         x.h = int.Parse(Console.ReadLine());
 
         Console.WriteLine(x.ValorEntrada());
@@ -17,13 +24,14 @@ class Cinema {
 
     public string d;
     public int h;
-    public int op1 = 16;
-    public int op2 = 8;
-    public int op3 = 20;
+    
+    int op1 = 16;
+    int op2 = 8;
+    int op3 = 20;
 
     public double ValorEntrada() {
 
-        if (h > 0 && h < 17)
+        if (h != 0 && h < 17)
         {
             if (d == "segunda" || d == "terça" || d == "quinta")
                 return op1;
@@ -36,11 +44,11 @@ class Cinema {
         else
         {
             if (d == "segunda" || d == "terça" || d == "quinta")
-                return op1 + (50 / 100) * op1;
+                return op1 + (50 / 100.0) * op1;
             else if (d == "quarta")
-                return op2 + (50 / 100) * op2;
+                return op2 + (50 / 100.0) * op2;
             else
-                return op3 + (50 / 100) * op3;
+                return op3 + (50 / 100.0) * op3;
         }
         
     }
