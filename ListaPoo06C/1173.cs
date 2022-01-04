23b949f4cf02d4f1d141826e66e2a38776148f0a
Antoniella Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2022-01-04 21:23:03 +0000

    Update main.exe

---
+++ ListaPoo06C/1173.cs
@@ -0,0 +1,24 @@
+using System;
+
+class MainClass {
+
+    public static void Main(string[] args) {
+
+        int v; // input value
+        int x; // array values
+        int[] n = new int[10];
+
+        v = int.Parse(Console.ReadLine());
+
+        x = v;
+    
+        for (int i = 0; i < 10; i++)
+        {
+            n[i] = x;
+            x = x * 2;
+
+            Console.WriteLine($"N[{i}] = {n[i]}");
+        }
+
+    }
+}
