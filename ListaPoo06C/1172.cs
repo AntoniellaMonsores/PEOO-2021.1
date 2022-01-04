8feb88a9d1e4c99a4dc5de32d9f92a2f4bc8c000
Antoniella Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2022-01-04 19:20:32 +0000

    Update .replit

---
+++ ListaPoo06C/1172.cs
@@ -0,0 +1,24 @@
+using system;
+
+class MainClass {
+
+    public static void Main(string[] args) {
+
+        int[] x = new int[10] x;
+        int n;
+
+        for (int i = 0; i < 10; i++)
+        {
+            n = int.Parse(Console.ReadLine());
+
+            if (n <= 0)
+            {
+                n = 1;
+            }
+
+            x[i] = n
+        }
+
+    }
+
+}
