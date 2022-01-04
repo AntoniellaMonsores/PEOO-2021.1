6a86a4b9079a71501ee9f2ee39f93286d926f366
Antoniella Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2022-01-04 21:42:12 +0000

    Update main.exe

---
+++ ListaPoo06C/1174.cs
@@ -0,0 +1,23 @@
+using System;
+
+class MainClass 
+{
+    public static void Main(string[] args)
+    {
+        double[] array = new double[100];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = double.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] <= 10)
+            {
+                Console.WriteLine($"A[{i}] = {array[i]:0.0}");
+            }
+        }
+    }
+    
+}
