c049036f24915aa332b407129ce61c929988a801
Antoniella de Araújo Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2022-01-05 18:29:11 -0300

    Update ex01.cs

--- ListaPoo02/ex01.cs
+++ ListaPoo02/ex01.cs
@@ -4,13 +4,13 @@ class Program {
 
     public static void Main(string[] args) {
     
-        Circulo x = new Circulo();
+        Circulo x = new Circulo(); // instanciando x
         
-        x.SetRaio(2);
+        x.SetRaio(2); // valor do raio
         
         double area = x.CalcArea();
         double circ = x.CalcCircunferencia();
-
+        
         Console.WriteLine($"Área do círculo: {area:0.00}");
         Console.WriteLine($"Circunferência do círculo: {circ:0.00}");
     }
@@ -19,14 +19,18 @@ class Program {
 
 class Circulo {
 
-    private double raio;
+    private double raio; // valor recebido da classe Main - definido pelo usuário
     double pi = 3.14159;
 
     public void SetRaio(double v) {
-        if (v >= 1) raio = v;
+        // teste com o valor do raio
+        // os cálculos apenas serão possíveis com raio maior que zero
+        if (v > 0) raio = v;
     }
-  
+
     public double GetRaio() {
+        // retorno do valor
+        // poderá ser utilizado na classe Main
         return raio;
     }
 
