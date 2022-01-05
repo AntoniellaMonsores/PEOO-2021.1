55cab806111045c1f2dfaebc6681598e4c832e42
Antoniella de Araújo Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2022-01-05 18:39:45 -0300

    Update ex02.cs

--- ListaPoo02/ex02.cs
+++ ListaPoo02/ex02.cs
@@ -1,1 +1,22 @@
+using System;
 
+class Program 
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
+
+class Disciplina
+{
+    private string nome;
+    private int nota1;
+    private int nota2;
+    private int nota3;
+    private int nota4;
+    int p1 = 2; // peso notas 1° semestre
+    int p2 = 3; // peso notas 2° semestre
+    
+    
+}
