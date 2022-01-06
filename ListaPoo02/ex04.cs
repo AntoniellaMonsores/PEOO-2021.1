d28d4995408fe507b70e81d25b95285b8dabb5b1
Antoniella de Araújo Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2022-01-06 11:08:19 +0000

    Update main.exe

--- ListaPoo02/ex04.cs
+++ ListaPoo02/ex04.cs
@@ -6,11 +6,12 @@ class Program {
 
         Cinema a = new Cinema();
 
-        a.SetDia(7);
-        a.SetHora(12.5);
+        a.SetDia(1);
+        a.SetHora(17.5);
 
-        Console.WriteLine(a.ValorEntrada());
-
+        Console.WriteLine($"Dia: {a.GetDia()}");
+        Console.WriteLine($"Horário: {(int)a.GetHora()} h e {a.GetHora() % 1 * 60} min");
+        Console.WriteLine($"Entrada: R$ {a.ValorEntrada():0.00}");
     }
 
 }
@@ -56,15 +57,15 @@ class Cinema {
         else 
         {
             if (dia == 2 || dia == 3 || dia == 5) { // segunda, terça e quinta
-                return 16 + (50 / 100) * 16;
+                return 16 + (50 / 100.0) * 16;
             }
             
             else if (dia == 4) { // quarta
-                return 8 + (50 / 100) * 20;
+                return 8 + (50 / 100.0) * 20;
             }
     
             else if (dia == 1 || dia == 6 || dia == 7) { // domingo, sexta e sábado
-                return 20 + (50 / 100) * 20;
+                return 20 + (50 / 100.0) * 20;
             }
         }
         
