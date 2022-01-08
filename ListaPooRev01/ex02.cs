9bc3f54dde7ff77970f3d4a87ceaf8dfb04f22d7
Antoniella de Araújo Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2022-01-08 07:28:43 -0300

    Create ex02.cs

---
+++ ListaPooRev01/ex02.cs
@@ -0,0 +1,12 @@
+
+
+/* Informe o número de países: 
+Informe o nome do 1o país: 
+Informe a população do 1o país: 
+Informe a área geográfica do 1o país em km2
+: 
+
+Informe o nome do 2o país:
+Informe a população do 2o país: 
+Informe a área geográfica do 2o país em km2
+:*/
