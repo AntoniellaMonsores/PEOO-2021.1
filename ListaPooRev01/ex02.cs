e787387be2656aaae10b59ffbf85ba2aca9aafea
Antoniella de Araújo Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2022-01-08 07:33:15 -0300

    Update ex02.cs

--- ListaPooRev01/ex02.cs
+++ ListaPooRev01/ex02.cs
@@ -1,5 +1,3 @@
-
-
 /* Informe o número de países: 
 Informe o nome do 1o país: 
 Informe a população do 1o país: 
@@ -10,3 +8,32 @@ Informe o nome do 2o país:
 Informe a população do 2o país: 
 Informe a área geográfica do 2o país em km2
 :*/
+
+using System;
+
+class Program {
+    
+    public static void Main(string[] args) {
+        
+        
+        
+    }
+    
+}
+
+class Pais {
+
+    private nome;
+    private int populacao;
+    private double area;
+    
+    public void SetPopulacao(int p) {
+        if (p > 0) populacao = p;
+    }
+
+    public void SetArea(double a) {
+        if (a > 0) area = a;
+    }
+    
+    
+}
