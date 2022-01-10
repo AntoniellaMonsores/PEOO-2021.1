c27f03acbcd014f0f7d46eb9159bc3f0d6e1b5dc
Antoniella de Araújo Monsores <78799880+AntoniellaMonsores@users.noreply.github.com>
2022-01-10 13:13:34 +0000

    Update ex02.cs

--- ListaPooRev01/ex02.cs
+++ ListaPooRev01/ex02.cs
@@ -11,18 +11,14 @@ class Program {
 
         for (int i = 0; i < vetor.Length; i++)
         {
-            string nome;
-            double area;
-            int habs;
-            
             Console.Write($"Informe o nome do {i+1}° país: ");
-            nome = Console.ReadLine();
+            string nome = Console.ReadLine();
             
             Console.Write($"Informe a população do {i+1}° país: ");
-            habs = int.Parse(Console.ReadLine());
+            int habs = int.Parse(Console.ReadLine());
             
             Console.Write($"Informe a área geográfica do {i+1}° país em km²: ");
-            area = double.Parse(Console.ReadLine());
+            double area = double.Parse(Console.ReadLine());
             
             Pais p = new Pais(nome, habs, area);
             vetor[i] = p;
@@ -31,14 +27,21 @@ class Program {
         int[] h = new int[n];
         double[] a = new double[n];
 
-        int ind1 = vetor.MaiorPopulacao(n, h);
-
         for (int i = 0; i < n; i++) {
             h[i] = vetor[i].GetPopulacao();
             a[i] = vetor[i].GetArea();
         }
-        
-        Console.WriteLine($"Mais populoso: {vetor[ind1].GetNome()} - {vetor[ind1].GetPopulacao()}");
+
+        int ind1 = Pais.MaiorPopulacao(n, h);
+        int ind2 = Pais.MaiorArea(n, a);
+
+        string n_max1 = vetor[ind1].GetNome();
+        string n_max2 = vetor[ind2].GetNome();
+        double = a_max = vetor[ind2].GetArea();
+        int p_max = vetor[ind1].GetPopulacao();
+
+        Console.WriteLine($"Mais populoso: {n_max1} - {p_max} habitantes");
+        Console.WriteLine($"Maior área: {n_max2} - {a_max}" km²);
     }
 
 }
@@ -67,7 +70,8 @@ class Pais {
         return area;
     }
     
-    public int MaiorPopulacao(int n, int[] h) {
+    public static int MaiorPopulacao(int n, int[] h) 
+    {
         int max = 0;
         int ind = 0;
         
@@ -83,4 +87,20 @@ class Pais {
         return ind;
     }
     
+    public static int MaiorArea(int n, double[] a) 
+    {
+        double max = 0;
+        int ind = 0;
+        
+        for (int i = 0; i < n; i++) 
+        {
+            if (max < a[i])
+            {
+                max = a[i];
+                ind = i;
+            }
+        }
+        
+        return ind;
+    }
 }
